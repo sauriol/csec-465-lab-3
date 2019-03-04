7dbb5c88dc6ca77208c06a5e286c8b424eebef21
Jack Sauriol <sauriol@csh.rit.edu>
2019-03-03 21:18:18 -0500

    Added quotes mostly

--- 3/ping-sweep.sh
+++ 3/ping-sweep.sh
@@ -7,12 +7,12 @@ usage() {
 
 ip_to_int() {
     local ip="$1"
-    local w=$(echo $ip | cut -d. -f1)
-    local x=$(echo $ip | cut -d. -f2)
-    local y=$(echo $ip | cut -d. -f3)
-    local z=$(echo $ip | cut -d. -f4)
+    local w=$(echo "$ip" | cut -d. -f1)
+    local x=$(echo "$ip" | cut -d. -f2)
+    local y=$(echo "$ip" | cut -d. -f3)
+    local z=$(echo "$ip" | cut -d. -f4)
 
-    local int=$((256 * 256 * 256 * $w + 256 * 256 * $x + 256 * $y + $z))
+    local int=$((256 * 256 * 256 * w + 256 * 256 * x + 256 * y + z))
 
     echo "$int"
 }
@@ -20,16 +20,16 @@ ip_to_int() {
 int_to_ip() {
     local int="$1"
 
-    local d=$(($int % 256))
-    local c=$((($int - $d) / 256 % 256))
-    local b=$((($int - $c - $d) / (256 * 256) % 256))
-    local a=$((($int - $c - $d - $b) / (256 * 256 * 256) % 256))
+    local d=$((int % 256))
+    local c=$(((int - d) / 256 % 256))
+    local b=$(((int - c - d) / (256 * 256) % 256))
+    local a=$(((int - c - d - b) / (256 * 256 * 256) % 256))
 
     echo "$a.$b.$c.$d"
 }
 
 ping_host() {
-    timeout $timeout ping -c1 $1 1>/dev/null && echo $1
+    timeout "$timeout" ping -c1 "$1" 1>/dev/null && echo "$1"
 }
 
 if [ $# -ne 1 ]; then
@@ -43,26 +43,26 @@ range="^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}-[0-9]{1,3}\.[0-9]{1,3}\.[
 cidr="^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}$"
 
 if [[ "$1" =~ $ip ]]; then
-    ping_host $1
+    ping_host "$1"
 elif [[ "$1" =~ $range ]]; then
-    begin=$(echo $1 | cut -d- -f1)
-    end=$(echo $1 | cut -d- -f2)
+    begin=$(echo "$1" | cut -d- -f1)
+    end=$(echo "$1" | cut -d- -f2)
 
-    first=$(ip_to_int $begin)
-    last=$(ip_to_int $end)
+    first=$(ip_to_int "$begin")
+    last=$(ip_to_int "$end")
 
-    for int in $(seq $first $last); do
-        ping_host $(int_to_ip $int)
+    for int in $(seq "$first" "$last"); do
+        ping_host "$(int_to_ip "$int")"
     done
 elif [[ $1 =~ $cidr ]]; then
-    net=$(echo $1 | cut -d/ -f1)
-    mask=$(echo $1 | cut -d/ -f2)
+    net=$(echo "$1" | cut -d/ -f1)
+    mask=$(echo "$1" | cut -d/ -f2)
 
-    first=$(ip_to_int $net)
+    first=$(ip_to_int "$net")
     last=$(( first | ((1 << (32 - mask)) - 1)))
 
-    for int in $(seq $first $last); do
-        ping_host $(int_to_ip $int)
+    for int in $(seq "$first" "$last"); do
+        ping_host "$(int_to_ip "$int")"
     done
 else
     usage
