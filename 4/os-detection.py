303d42efb616fe5860f86ed8f07c78bab81f0dad
Jack Sauriol <sauriol@csh.rit.edu>
2019-03-04 18:04:52 -0500

    Fingerprinting based on TTL

--- 4/os-detection.py
+++ 4/os-detection.py
@@ -14,7 +14,16 @@ def main():
     addrs = f.read().strip().split('\n')
 
     for addr in addrs:
-        print(addr)
+        sent = IP(dst=addr)/ICMP()
+        rep = sr1(IP(dst=addr)/ICMP(code=61), timeout=1, verbose=0)
+
+        if rep:
+            print(addr + ':')
+
+            if rep.ttl > 64 and rep.ttl <= 128:
+                print('\tWindows')
+            elif rep.ttl > 32 and rep.ttl <= 64:
+                print('\tLinux')
 
 
 
