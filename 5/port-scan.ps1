da929f79b6137c965aec8603cd0ce9c192e04278
Dan Lennon <47697337+djl9490@users.noreply.github.com>
2019-03-07 17:36:49 -0500

    Hard coded values...
    Hard coded values for when given a range of IP Addresses that will display up to port 500. Also displays if addresses have a connection or not.

--- 5/port-scan.ps1
+++ 5/port-scan.ps1
@@ -1,21 +1,47 @@
-[CmdletBinding()]
-param(
-    [Parameter(
-        ValueFromPipelineByPropertyName=$true
-        ]
-    [ValidateScript({$_ -match [IPAddress]$_ })]
-    [string]$IPAddress,
+$IPone = "129.21.137.172".Split('.') 
+$IPtwo = "129.21.137.200".Split('.') 
 
-    [Parameter(
-        ValueFromPipeline=$true
-    )]
-    [ValidateScript({})]
-    [string]$IPAdressRange,
+$portRange = 1..500
+$timeout_ms = 5
 
-    [Parameter(
-        ValueFromPipeline=$true
-    )]
-    [ValidateRange(1,65535)]
-    [Int32]$Port
+$IPrange = ([int]$IPone[3])..([int]$IPtwo[3])
 
-)
+foreach($r in $IPrange)
+{
+    $lastOctect = [String]($r)
+    $ip = "{0}.{1}.{2}.{3}" -F $IPone[0],$IPone[1],$IPone[2],$lastOctect
+
+    if (Test-Connection -BufferSize 32 -Count 1 -Quiet -ComputerName $ip)
+    {
+        " "
+        "=========================================================================="
+        Write-Host "IP $ip is alive... checking ports..."
+
+        foreach ($port in $portRange)
+        {
+            $ErrorActionPreference = 'SilentlyContinue'
+            $socket = new-object System.Net.Sockets.TcpClient
+            $connect = $socket.BeginConnect($ip, $port, $null, $null)
+            $tryconnect = Measure-Command { $success = $connect.AsyncWaitHandle.
+            WaitOne($timeout_ms, $true) } | % totalmilliseconds
+            $tryconnect | Out-Null
+
+            if ($socket.Connected)
+            {
+                "$ip is listening on port $port (Response Time: $tryconnect ms)"
+                $socket.Close()
+                $socket.Dispose()
+                $socket = $null
+            }
+            $ErrorActionPreference = 'Continue'
+        }
+        "=========================================================================="
+    }
+    else 
+    {
+        " "
+        "=========================================================================="
+        Write-Host "IP $ip has no connection..."
+        "=========================================================================="
+    }
+}
